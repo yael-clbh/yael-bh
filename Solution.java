33f6f3099cc11a80e4543d1b1517eb49a376b6a3
yael-clbh <62343767+yael-clbh@users.noreply.github.com>
2020-03-19 10:15:00 +0200

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,83 @@
+package exam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.lang.Math;
+
+class Solution {
+	
+	public int solution (int N) {
+   	String str = Integer.toString(N);
+	int temp = N;
+	List<Integer> list = new ArrayList<>();
+	int i =0;
+	while (i<str.length()) {
+		list.add(0,temp % 10);
+		temp /=10;
+		i+=1;
+	}
+	Object[] ints = list.toArray();
+	for (int j = 0; j<ints.length; j++) {
+		if ((int)ints[j] <5 ) {
+			list.add(j,5);
+		}
+	}
+	int ans = 0;
+	Object[] ansArray = list.toArray();
+	for(int k=0; k<ansArray.length; k++) {
+		ans += (int)ansArray[k]* Math.pow(10, ansArray.length-k-1);
+	}
+	return ans;
+}
+	
+	public int solution (String S) {
+		char [] chars = S.toCharArray();
+		List <Character> l = new ArrayList<>(); 
+		int count = 0;
+		for (int i =0; i<chars.length;i++) {
+			l.add(chars[i]);
+		}
+		if (l.get(0) == 'a') {
+			if (l.get(1)!='a') {
+				l.add(0,'a');
+				count +=1;
+			}
+		}
+		else {
+			l.add(0,'a');
+			l.add(0,'a');
+			count +=1;
+		}
+		if (l.get(l.size()-1)!='a') {
+			count +=2;
+		}
+		else {
+			if (l.get(l.size()-2)!= 'a'){
+					count +=1;
+			}
+		}
+		for (int j =1; j<l.size()-1;j++) {
+			if (l.get(j) == 'a') {
+				if(l.get(j-1)!='a') {
+					if(l.get(j+1)!= 'a') {
+					l.add(j+1, 'a');
+					count+=1;
+				}
+				}
+				else {
+					if (l.get(j+1) =='a') {
+						System.out.println(l);
+						return -1;
+						}
+					}
+			}
+			else {
+				l.add(j+1,'a');
+				count +=1;
+			}
+		}
+	return count;
+	}
+		
+}
